d2caf64c753e02ade1733511710afe66a52a6089
Stuti Sinha <stutisinha712@gmail.com>
2025-06-25 12:11:56 +0530

    Update Loading component: enhance background styling with blur effect for improved visual appeal

--- matrisamman/src/components/Landing/loading.jsx
+++ matrisamman/src/components/Landing/loading.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 function Loading() {
   return (
-    <div className="min-h-screen w-full flex items-center justify-center bg-[#5a0404]">
+    <div className="min-h-screen w-full flex items-center justify-center bg-[#5a0404]/90 backdrop-blur-lg">
       <img
         src="/logo2.png"
         alt="Matri Samman Logo"
